13240b321062d62b5e9770469d7f6c4812526f5b
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2021-09-08 00:15:03 +0900

    Set up 2
    server, pug, static complete

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-console.log("Hi")
+import { join } from "path";
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+const handleListening = () => {
+    console.log(`✅ Server running: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
