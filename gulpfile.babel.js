72da6ccf1b36394c68222dafdeecbd7374b730cd
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2021-09-12 01:50:28 +0900

    gulp setup 2
    'gulp-autoprefixer' : 여러 브라우저 호환 가능
    'gulp-csso' : .css 압축
    gulp.watch : 매번 업데이트 해야하는 번거로움 없앰

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,35 @@
 import gulp from "gulp";
 import gulpSass from "gulp-sass";
 import nodeSass from "node-sass";
+import autoprefixer from 'gulp-autoprefixer';
+import minifyCSS from "gulp-csso";
+
 const sass = gulpSass(nodeSass);
 
 const paths = {
     styles: {
         src: "asset/scss/style.scss",
         dest: "src/static/styles",
+        watch: "asset/scss/**/**.scss",
     }
 }
 
-export function styles() {
+function styles() {
     return gulp
         .src(paths.styles.src)
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(minifyCSS())
         .pipe(gulp.dest(paths.styles.dest));
 }
+
+/* 매번 업데이트 해야하는 번거로움 감소 */
+function watchFiles() {
+    gulp.watch(paths.styles.watch, styles);
+}
+
+const dev = gulp.series([styles, watchFiles]);
+
+export default dev;
